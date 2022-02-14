4f6d07ce96375846a51a417c2902c6b4cf56c83f
daniel <daniel.gj.tan@gmail.com>
2022-02-14 23:16:02 +0800

    fix migrate

--- core/dbx/migrate.go
+++ core/dbx/migrate.go
@@ -3,6 +3,7 @@ package dbx
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	"gorm.io/gorm"
 )
@@ -18,30 +19,14 @@ func RegisterModel(m interface{}, initialValue ...interface{}) {
 }
 
 func PGMigrate(ctx context.Context, tenants ...string) error {
-	sessionDB := DB.Session(&gorm.Session{
-		NewDB:   true,
-		Context: ctx,
-	})
-
-	for _, tenant := range tenants {
-		for i, v := range _registeredModel {
-			if err := sessionDB.Exec(fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS \"%v\" ;", tenant)).Error; err != nil {
-				fmt.Println(err)
-			}
-			if err := sessionDB.Scopes(WithTenant(tenant, v)).AutoMigrate(v); err != nil {
-				fmt.Println(err)
-			}
-			for _, value := range _registeredValue[i] {
-				if err := sessionDB.Scopes(WithTenant(tenant, v)).FirstOrCreate(value).Error; err != nil {
-					fmt.Println(err)
-				}
-			}
-		}
-	}
-	return nil
+	return migrate(ctx, "CREATE SCHEMA IF NOT EXISTS \"%v\" ;", tenants...)
 }
 
 func MysqlMigrate(ctx context.Context, tenants ...string) error {
+	return migrate(ctx, "CREATE SCHEMA IF NOT EXISTS `%v` ;", tenants...)
+}
+
+func migrate(ctx context.Context, sql string, tenants ...string) error {
 	sessionDB := DB.Session(&gorm.Session{
 		NewDB:   true,
 		Context: ctx,
@@ -49,14 +34,14 @@ func MysqlMigrate(ctx context.Context, tenants ...string) error {
 
 	for _, tenant := range tenants {
 		for i, v := range _registeredModel {
-			if err := sessionDB.Exec(fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS `%v` ;", tenant)).Error; err != nil {
+			if err := sessionDB.Exec(fmt.Sprintf(sql, tenant)).Error; err != nil {
 				fmt.Println(err)
 			}
 			if err := sessionDB.Scopes(WithTenant(tenant, v)).AutoMigrate(v); err != nil {
 				fmt.Println(err)
 			}
 			for _, value := range _registeredValue[i] {
-				if err := sessionDB.Scopes(WithTenant(tenant, v)).FirstOrCreate(value).Error; err != nil {
+				if err := sessionDB.Scopes(WithTenant(tenant, reflect.New(reflect.TypeOf(v).Elem()).Interface())).FirstOrCreate(reflect.New(reflect.TypeOf(v).Elem()).Interface(), value).Error; err != nil {
 					fmt.Println(err)
 				}
 			}
